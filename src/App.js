c5e09a308e9a8a23e16d53b4bb701ab9a889200d
lappi357 <lappi357@gmail.com>
2021-11-01 11:15:59 +0530

    changing the context

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Production Env is done.
         </p>
         <a
           className="App-link"
